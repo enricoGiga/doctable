c476ae1a33b37f389072e59c1a43ee8459c61471
enricoGiga <enrico.gigante@gmail.co>
2023-11-28 10:55:37 +0100

    add demo with streamlit

--- doctable/table_recognition/recognition.py
+++ doctable/table_recognition/recognition.py
@@ -7,11 +7,27 @@ from doctable.data_utility.utilities import cv2_read
 
 
 class TableRecognizer:
+    """
+    The TableRecognizer class is responsible for recognizing tables in a given image.
+    It uses the PPStructure class to perform the recognition.
+    """
 
     def __init__(self):
+        """
+        Initializes the TableRecognizer class with a PPStructure.
+        """
         self.ppstructure = PPStructure()
 
     def recognize(self, cropped_image: Union[str, np.array]):
+        """
+        Recognizes tables in a given image. The image can be a path to an image file or a numpy array.
+
+        Args:
+            cropped_image (Union[str, np.array]): The image to recognize tables in.
+
+        Returns:
+            The result of the table recognition.
+        """
         if isinstance(cropped_image, str):
             cropped_image = cv2_read(cropped_image)
 
